a12eb4cc9657828d038e9fddd974a6c2cb3e75e3
Nataliia Sokolova <natalia.sokolova.ca@gmail.com>
2023-06-02 09:57:17 -0700

    Add function

---
+++ script.js
@@ -0,0 +1,29 @@
+let fighters = [
+  "🐉",
+  "🐥",
+  "🐊",
+  "💩",
+  "🦍",
+  "🐢",
+  "🐩",
+  "🦭",
+  "🦀",
+  "🐝",
+  "🤖",
+  "🐘",
+  "🐸",
+  "🕷",
+  "🐆",
+  "🦕",
+  "🦁",
+];
+
+let stageEl = document.getElementById("stage");
+let fightButton = document.getElementById("fightButton");
+
+fightButton.addEventListener("click", function () {
+  stageEl.textContent = `${getRandomItem()} vs ${getRandomItem()}`;
+});
+
+const getRandomItem = () =>
+  fighters[Math.floor(Math.random() * fighters.length)];
